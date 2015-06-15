83576ff01a7d0b63562b1052f636ef9ecd8d01ef
Dara Clare <daraclare@gmail.com>
2015-06-15 22:03:02 +0100

    almost completed form

--- inc/trial.php
+++ inc/trial.php
@@ -1,20 +1,30 @@
 <?php
 
 	// Configuration 
-    $to = "name@example.com"; // Your email address. 
-    $subject = "New trial signup via the Signup Landing Page"; // Email subject line 
+    $to = "daraclare@gmail.com"; // Your email address. 
+    $subject = "Online Registration Form: Safer Discos & Socials for Galway Teens"; // Email subject line 
     
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $phone = $_REQUEST['phone'];
+    $membership = $_REQUEST['membership'];
+    $involvement = $_REQUEST['involvement-option1'];
+    $involvement = $_REQUEST['involvement-option2'];
+    $involvement = $_REQUEST['involvement-option3'];
+    $involvement = $_REQUEST['involvement-option4'];
     $message = $_REQUEST['message'];
     $headers = "From: $from";
 
     $fields = array();
     $fields{"name"} = "Name";
-    $fields{"email"} = "Email";
     $fields{"phone"} = "Phone";
-    $fields{"message"} = "Message";
+    $fields{"email"} = "Email";
+    $fields{"membership"} = "Membership";
+    $fields{"involvement-option1"} = "Involvement Option 1";
+    $fields{"involvement-option2"} = "Involvement Option 2";
+    $fields{"involvement-option3"} = "Involvement Option 3";
+    $fields{"involvement-option4"} = "Involvement Option 4";
+    $fields{"message"} = "Comments";
 
     $body = "Details:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
 
